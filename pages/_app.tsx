48fa11cfb381c0177923802f8a5c3a898594c25d
bas <755jkm@gmail.com>
2025-04-19 21:34:56 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+// This file is used to initialize pages. You can override it and control the page initialization.
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
